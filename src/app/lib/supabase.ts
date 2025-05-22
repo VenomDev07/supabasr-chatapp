8cc2596741526f47ea411826573d36436cf116b2
Devendra Salokhe <devendra.salokhe2003@gmail.com>
2025-05-22 20:28:20 +0530

    changes made for api keys

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://xvoyqpuupznpcfxoguha.supabase.co" as string;
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2b3lxcHV1cHpucGNmeG9ndWhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTQyOTUsImV4cCI6MjA2MzMzMDI5NX0.507Us10Zn_MZ9mLMELf9kORgjByE91DUq5tkDqbVfCI" as string;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
